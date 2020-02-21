d89d1c08f58756aa3b4356151f63c1eac187818b
darkwebplayer <amrithanadhm@gmail.com>
2020-02-21 15:29:44 +0530

    added bottom bar and tweaks to menu

--- js/main.js
+++ js/main.js
@@ -2,35 +2,36 @@
 const hamburger = document.querySelector(".hamburger");
 const hamLines = document.querySelectorAll(".line");
 const navList = document.querySelector(".nav-list");
-hamburger.addEventListener("click",()=>{
-    hamLines.forEach((hamLine)=>{
-        hamLine.classList.toggle("change")
-        navList.classList.toggle("change")
-    });
+hamburger.addEventListener("click", () => {
+  hamLines.forEach((hamLine) => {
+    hamLine.classList.toggle("change")
+    navList.classList.toggle("change")
+  });
 });
 
 // Smooth scroll effect
-$(document).ready(function(){
-    // Add smooth scrolling to all links
-    $("a").on('click', function(event) {
-  
-      // Make sure this.hash has a value before overriding default behavior
-      if (this.hash !== "") {
-        // Prevent default anchor click behavior
-        event.preventDefault();
-  
-        // Store hash
-        var hash = this.hash;
-  
-        // Using jQuery's animate() method to add smooth page scroll
-        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-        $('html, body').animate({
-          scrollTop: $(hash).offset().top
-        }, 800, function(){
-  
-          // Add hash (#) to URL when done scrolling (default click behavior)
-          window.location.hash = hash;
-        });
-      } // End if
-    });
+$(document).ready(function () {
+  // Add smooth scrolling to all links
+  $("a").on('click', function (event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function () {
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
   });
+});
+
